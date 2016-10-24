9bb727366fbd0aaadcdb5024866cd95b45e5f63e
arafat5549 <arafat5549@gmail.com>
2016-10-24 23:45:01 +0800

    ALL

--- README.md
+++ README.md
@@ -1,29 +1,36 @@
 ### Maven(推荐) / Gradle / Ant:
 
-0.下载一个Maven
+0.下载一个Maven,下载地址如下：
 
-1.设置环境变量到bin路径
+http://mirrors.hust.edu.cn/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
 
-2.要使用Eclipse的话需要下载Eclipse的Maven插件
+1.设置环境变量到bin路径(命令行运行mvn -version查看是否安装成功)
 
-- M2E - Maven Integration for Eclipse
+2.要使用Eclipse的话需要下载Eclipse的Maven插件:
 
-3. 1. Mvn install
-   2. Mvn Test
-   3. Mvn eclipse:eclipse
+- http://download.eclipse.org/technology/m2e/releases/1.4
+- Eclipse- Help->Install New Software 输入上面那个地址
 
-4. 安装完插件后，Import ->ExsistingMavenProject
 
-5. 到POM.xml文件的那个路径
+- 插件名称:[M2E - Maven Integration for Eclipse]
 
-6. - src/main/java:源代码目录
+3. 1. 常用的Maven命令(到pom.xml所在的目录运行)
+   2. Mvn install
+   3. Mvn Test
+   4. Mvn eclipse:eclipse
+
+4. 安装完插件后，Import ->ExsistingMavenProject导入工程（POM.xml文件的那个路径）
+
+5. - src/main/java:源代码目录
    - src/test/java: 自动运行所有的单元测试类
    - src/main/resources:配置文件,classpath
    - src/main/webapp:WEB工程多一个这个目录
 
-7. maven安装路径\conf\settings.xml
+6. 阿里的国内Maven仓库镜像不然下载很慢：
 
-8. ```xml
+   maven安装路径 \conf\settings.xml
+
+7. ```xml
     <mirror>
          <id>alimaven</id>
          <name>aliyun maven</name>
@@ -32,14 +39,8 @@
        </mirror>
    ```
 
-9. ​
+8. 其他常用打包工具:
 
 
-
-Apache工具包：
-
-Google Guava工具包：
-
-ANI：AndroidNativeInterface
-
-//Gradle新的打包工具 Groovy
+- Apache工具包（不推荐）：不好用
+- Gradle（推荐度 4星）:安卓官方打包工具
--- src/test/java/foo/concurrent/ConcurrentDemo.java
+++ src/test/java/foo/concurrent/ConcurrentDemo.java
@@ -17,6 +17,57 @@ package foo.concurrent;
  */
 public class ConcurrentDemo 
 {
+	/**
+	 * 1.多线程并发的历史
+	 * #1.[SingleTask单任务系统]:单cpu计算机，一次只能执行一个程序(program)
+	 * #2.[MultiTask多任务系统]: 单cpu计算机，一次能执行多个程序(并不是真正的同时需要CPU调度)
+	 * #3.[MultiThreading多线程系统]:多CPU计算机，在一个程序program里可以有多个线程Thread，
+	 * 多个线程在同一个程序里执行，相当于多个CPU同时处理同一个程序
+	 * 
+	 * 当不同线程同时争夺同一个内存区域，比如一个线程要写数据 另一个线程要读数据就可能出现脏数据。
+	 * 
+	 * 多线程并发的优点：
+	 * #1.资源利用更加高效
+	 * #2.
+	 * 缺点：
+	 * #1.程序的设计更加复杂
+	 * #2.线程频繁切换会降低效率(当CPU从一个线程调度到另一个时，需要保存本地数据，程序指针等等)
+	 * #3.资源消耗增大(每开启一个线程，CPU都要消耗一定的资源来运行它)
+	 * 
+	 * 多线程并发模型非常类似于分布式系统架构(把每一个架构上的服务器看成一个线程)
+	 * 并发模型ConcurrentModel:
+	 * 模型#1.Parallel Workers(大部分Java并发包(java.util.concurrent)的实现策略都是)
+	 * 
+	 *            --> Worker
+	 * delegator  --> Worker
+	 *            --> Worker
+	 * 优点：
+	 * 易懂容易理解
+	 * 缺点：
+	 * -共享状态非常复杂难懂complex
+	 * -Job Ordering is Nondeterministic
+	 * 
+	 * 
+	 * persistent data structures (immutable数据类型)
+	 * 
+	 * 模型#2.Assembly Line流水线   (e.g. Reactive Systems, event driven systems)
+	 * 大部分是为了Non-BlockingIO 非阻塞IO
+	 * delegator  --> Worker --> Worker --> Worker
+	 * 
+	 * Each worker only performs a part of the full job. When that part is finished 
+	 * the worker forwards the job to the next worker.
+	 * 每个工人只完成整个Job的一部分，当完成的时候将Job传给下一个工人。
+	 * Each worker is running in its own thread, and shares no state with other workers. 
+	 * This is also sometimes referred to as a shared nothing concurrency model.
+	 * 每个工人在自己的线程跑，并且不跟其他工人共享状态。也叫作 状态不共享模型
+	 */
+	
+	
+	
+	
+	
+	
+	
     //1.现代GUI架构 (EventDispatchThread事件派发线程EDT)取代了主线程循环
 	//2.
 	//编写线程安全的代码，本质上是管理对状态state的访问，特别是共享，可变的状态。
